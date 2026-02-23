0fe6c672b22a2b84420e102d6211375850a9203c
Shivakumar Kirigeri <shivakumar641@gmail.com>
2026-02-23 19:11:41 +0530

    task:basic inputs to run

---
+++ src/app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const { connectDB } = require("./database/connectDB");
+const PORT = process.env.PORT || 8888;
+const app = express();
+
+/* 🔐 MUST be before CORS & cookies */
+//app.set("trust proxy", 1);
+
+/* Measure latency */
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on("finish", () => {
+    req.latency = Date.now() - start;
+  });
+  next();
+});
+
+app.use(express.json());
+
+/* ✅ CORS for cross-subdomain cookies */
+/*app.use(
+  cors({
+    origin: [
+      "https://serverpe.in",
+      "https://admin.serverpe.in",
+      "https://carspecs.serverpe.in",
+    ],
+    credentials: true,
+  })
+);*/
+app.use(
+  cors({
+    origin: "http://localhost:1234",
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+
+/* Health check */
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    service: "serverpe-plantgang API",
+    message: "API is running successfully 🚀",
+  });
+});
+
+/* Static files */
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+/* Routes */
+/* DB connections */
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
---
+++ src/database/connectDB.js
@@ -0,0 +1,40 @@
+const { Pool, types } = require("pg");
+require("dotenv").config();
+
+// Keep DATE as string (no timezone changes)
+types.setTypeParser(1082, (val) => val);
+
+// Global pools (created only once)
+let pool = null;
+
+// 🔥 Common function to test pool connection once
+function testConnection(pool, label) {
+  pool
+    .query("SELECT NOW()")
+    .then(() => console.log(`✅ PostgreSQL connected: ${label}`))
+    .catch((err) => console.error(`❌ Connection failed for ${label}`, err));
+}
+
+/* ============================================
+   MAIN DB (PGDATABASEMAIN)
+============================================ */
+const connectDB = () => {
+  if (!poolmain) {
+    poolmain = new Pool({
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASEMAIN,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+      max: 20,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+      keepAlive: true,
+    });
+
+    testConnection(poolmain, "serverpe-plantgang DB");
+  }
+  return poolmain;
+};
+
+module.exports = { connectDB };
